39098464d4fbea95bfb381f72725a980e7fe9e19
priyangshubanerjee2108-star <priyangshubanerjee2108@gmail.com>
2026-08-08 18:27:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN === "*" ? true : process.env.CLIENT_ORIGIN,
+    origin: true,
   })
 );
 app.use(express.json({ limit: "1mb" }));
